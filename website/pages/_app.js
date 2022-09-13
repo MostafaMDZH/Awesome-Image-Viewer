32dd322636c962cd28753001b7fea5ef6ba7154f
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2022-09-13 15:07:14 +0430

    website: module css import removed

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/index.scss'
 import '../styles/main.scss'
 import '../styles/main.responsive.scss'
-import '../node_modules/awesome-image-viewer/src/imageViewer.scss' //uncomment at dev mode
+// import '../node_modules/awesome-image-viewer/src/imageViewer.scss' //uncomment at dev mode
 
 export default function MyApp({ Component, pageProps }){
     return <Component {...pageProps} />
