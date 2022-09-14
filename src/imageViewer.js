f43fd8f9e95955e091093521c0c0480a085507be
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2022-09-14 11:49:56 +0430

    [deployment auto commit]

--- src/imageViewer.js
+++ src/imageViewer.js
@@ -37,7 +37,7 @@ class ImageViewer {
             direction === 'RIGHT' ? index-- : index++;
             setTimeout(() => {
                 this.selectImage(index);
-            }, 5);
+            }, 15);
         }, () => this.selectImage(this.currentSelected));
         //hud and zoom events:
         this.addEventToHudAndZoom();
