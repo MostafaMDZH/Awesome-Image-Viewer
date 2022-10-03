ec01919bb9288fc6ce2ff2ec9837919dd8302009
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2022-10-03 10:55:57 +0330

    [deployment auto commit]

---
+++ website/out/_next/static/qT1DFC0M0BnJqutkx7DKP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/16-0b4d78ff46087066.js","static/chunks/pages/index-3fea84d39a8ea187.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
