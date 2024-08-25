33c59c5822d6d3da084a3eca712da5a5899dc1f7
Mostafa Mohammadzadeh <mostafa.mdzh@gmail.com>
2024-08-25 16:36:43 +0330

    c

---
+++ website/out/_next/static/CM9Fxiapr4wFHCLrcy56j/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/16-4f01535df62a4a79.js","static/chunks/pages/index-66836bfd94cf303f.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
